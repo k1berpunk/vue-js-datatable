dfb79bc6a8198d01e1e87303549454fc01e01688
Dmitry Khalichev <khalichev@gmail.com>
2020-10-28 16:06:28 +0300

    Build added

---
+++ dist/VueJsDatatable.esm.js
@@ -0,0 +1,263 @@
+import datatable from 'datatables.net';
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+if (typeof global.$ === "undefined") {
+	global.$ = require( 'jquery' );
+}
+if (typeof global.jQuery === "undefined") {
+	global.jQuery = require( 'jquery' );
+}
+global.$.fn.DataTable = datatable;
+
+var script = {    
+	name: 'VueJsDatatable',
+	props: {
+		columns: {
+			type: Array|null,
+			default: function () { return []; }
+		},
+		url: {
+			type: String,
+			default: null	
+		},
+		type: {
+			type: String,
+			default: 'GET'
+		},
+		searching: {
+			type: Boolean,
+			default: true
+		},
+		lengthChange: {
+			type: Boolean,
+			default: true
+		},
+		order: {
+			type: Array,
+			default: function () { return []; }
+		},
+		className: {
+			type: String,
+			default: ''
+		},
+        paging: {
+            type: Boolean,
+            default: true
+        },
+        ordering: {
+            type: Boolean,
+            default: true
+        },
+        serverSide: {
+            type: Boolean,
+            default: true
+		},
+		headers: {
+			type: Object,
+			default: function () {}
+		}
+	},
+	data: function data() {
+		return {
+			datatable: null,
+		}
+	},
+	mounted: function mounted() {
+		var this$1 = this;
+
+		var that = this;
+		this.datatable = $(this.$el).DataTable({
+		    "paging": this.paging,
+		    "lengthChange": this.lengthChange,
+		    "searching": this.searching,
+		    "ordering": this.ordering,
+		    "info": true,
+		    "responsive": true,
+		    "processing": true,
+			"serverSide": this.serverSide,
+			"order": this.order,
+		    ajax: {
+		        'url': this.url,
+		        'type' : this.type,
+		        "beforeSend": function (xhr) {
+					if(this$1.headers) {
+						for (var key of Object.keys(this$1.headers)) {
+							xhr.setRequestHeader(key, this$1.headers[key]);
+						}
+					}
+		         }
+		    },
+	      	"columns": this.columns,
+	      	"drawCallback":function(setting){
+		        $('td [data-g-action]').click(function(e){
+		            e.preventDefault();
+		            
+		            var action = $(this).attr('data-g-action');
+		            
+		            var actionData = $(this).attr('data-g-actiondata');
+		            
+		            var args = {
+		            	action: action,
+		            	data: actionData
+		            };
+
+					that.$emit('gaction', args);
+		        });
+		    },
+	    });
+	},
+	methods: {
+		reload: function reload(url) {
+			this.datatable.ajax.url(url).load();
+		},
+		draw: function draw() {
+			this.datatable.draw();
+		}
+	}
+};
+
+function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier /* server only */, shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
+    if (typeof shadowMode !== 'boolean') {
+        createInjectorSSR = createInjector;
+        createInjector = shadowMode;
+        shadowMode = false;
+    }
+    // Vue.extend constructor export interop.
+    var options = typeof script === 'function' ? script.options : script;
+    // render functions
+    if (template && template.render) {
+        options.render = template.render;
+        options.staticRenderFns = template.staticRenderFns;
+        options._compiled = true;
+        // functional template
+        if (isFunctionalTemplate) {
+            options.functional = true;
+        }
+    }
+    // scopedId
+    if (scopeId) {
+        options._scopeId = scopeId;
+    }
+    var hook;
+    if (moduleIdentifier) {
+        // server build
+        hook = function (context) {
+            // 2.3 injection
+            context =
+                context || // cached call
+                    (this.$vnode && this.$vnode.ssrContext) || // stateful
+                    (this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext); // functional
+            // 2.2 with runInNewContext: true
+            if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+                context = __VUE_SSR_CONTEXT__;
+            }
+            // inject component styles
+            if (style) {
+                style.call(this, createInjectorSSR(context));
+            }
+            // register component module identifier for async chunk inference
+            if (context && context._registeredComponents) {
+                context._registeredComponents.add(moduleIdentifier);
+            }
+        };
+        // used by ssr in case component is cached and beforeCreate
+        // never gets called
+        options._ssrRegister = hook;
+    }
+    else if (style) {
+        hook = shadowMode
+            ? function (context) {
+                style.call(this, createInjectorShadow(context, this.$root.$options.shadowRoot));
+            }
+            : function (context) {
+                style.call(this, createInjector(context));
+            };
+    }
+    if (hook) {
+        if (options.functional) {
+            // register for functional component in vue file
+            var originalRender = options.render;
+            options.render = function renderWithStyleInjection(h, context) {
+                hook.call(context);
+                return originalRender(h, context);
+            };
+        }
+        else {
+            // inject component registration as beforeCreate hook
+            var existing = options.beforeCreate;
+            options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+        }
+    }
+    return script;
+}
+
+/* script */
+var __vue_script__ = script;
+
+/* template */
+var __vue_render__ = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('table',{staticClass:"table table-hover",class:_vm.className,attrs:{"align":"center","width":"100%"}},[_c('thead',[_c('tr',[_vm._t("default")],2)]),_vm._v(" "),_c('tbody')])};
+var __vue_staticRenderFns__ = [];
+
+  /* style */
+  var __vue_inject_styles__ = undefined;
+  /* scoped */
+  var __vue_scope_id__ = undefined;
+  /* module identifier */
+  var __vue_module_identifier__ = undefined;
+  /* functional template */
+  var __vue_is_functional_template__ = false;
+  /* style inject */
+  
+  /* style inject SSR */
+  
+  /* style inject shadow dom */
+  
+
+  
+  var __vue_component__ = /*#__PURE__*/normalizeComponent(
+    { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ },
+    __vue_inject_styles__,
+    __vue_script__,
+    __vue_scope_id__,
+    __vue_is_functional_template__,
+    __vue_module_identifier__,
+    false,
+    undefined,
+    undefined,
+    undefined
+  );
+
+function install(Vue) {
+  if (install.installed) { return; }
+  install.installed = true;
+  Vue.component("VueJsDatatable", __vue_component__);
+}
+
+var plugin = {
+  install: install
+};
+
+var GlobalVue = null;
+if (typeof window !== "undefined") {
+  GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+  GlobalVue = global.vue;
+}
+
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
+__vue_component__.install = install;
+
+export default __vue_component__;
